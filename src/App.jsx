8a84106879250f0d98ce3357b214fd822f476abe
Ranjeth <ranjethranganathan@gmail.com>
2024-05-29 12:57:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import Todolist from '../Todo/Todolist'
+import TodoDesign from '../Todo/TodoDesign'
+// import Todopractice from './todopractice/Todopractice'
+
+
+export default function App() {
+  return (
+    <div>
+      {/* <Todolist/> */}
+      <TodoDesign/>
+      {/* <Todopractice/> */}
+      
+    </div>
+  )
+}
